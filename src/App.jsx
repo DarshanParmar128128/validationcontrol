b52549b325bc7a9b6348d5af66284dd760a16e2c
darshanparmar128128 <darshanparmar128128@gmail.com>
2026-04-20 10:30:18 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>validation control</h1>
+    <h2>demo</h2>
+    <h2>demo</h2>
+    </>
+  )
+}
+
+export default App
