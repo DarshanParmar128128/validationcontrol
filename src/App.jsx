f9f488f2de4cfd44b28b780e820df068185d58d6
darshanparmar128128 <darshanparmar128128@gmail.com>
2026-04-20 16:00:47 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { use, useState } from 'react';
+import { GoogleLogin } from "@react-oauth/google";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+   const [count,setcount]= useState(0); 
   return (
     <>
-    <h1>validation control</h1>
-    <h2>demo</h2>
-    <h2>demo</h2>
-    <h2>demo</h2>
-    <h3>new update</h3>
-    
-    </>
-  )
+    <div className="login-container">
+      
+      <div className="login-box">
+        <h2>Welcome Back</h2>
+        <p>Please login to continue</p>
+
+        <GoogleLogin className='glog'
+          onSuccess={(credentialResponse) => {
+            console.log("Login Success:", credentialResponse);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+      </div>
+        <div className="divider">OR</div>
+
+        <label className="email">Email</label>
+        <input type="text" className="email-box" />
+         <br />
+        <label className="pass">Password</label>
+        <input type="password" className="password-box" />
+
+        <div className="forgot">
+          <a href="#">Forgot password?</a>
+        </div>
+        <button className="btn" onClick={()=>{setcount(count+1)}}>Sign in</button>
+
+        <div className="alredy" >
+          <p>Don't have an account? <a href="#">Sign up</a></p>
+        </div>
+       </div>
+       <p className='out' >{count}</p>
+       </>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <GoogleOAuthProvider clientId="623841906381-5rba76vkedft8k0embpjo7mrbnu5def4.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
 )
